1db997c1b2b7a8e6032e88dc83259eb53a22820c
m3b1u5 <dbatezat@gmail.com>
2024-12-27 22:03:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# ДЗ: Импорт Selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
