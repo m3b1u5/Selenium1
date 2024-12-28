65d577e0f3657fbaa3feb0961e807ac81db58529
m3b1u5 <dbatezat@gmail.com>
2024-12-28 16:07:28 +0200

    First test ensure URL ensure Title

--- Chrome.py
+++ Chrome.py
@@ -1,4 +1,4 @@
-# ДЗ: Импорт Selenium (Chrome)
+# ДЗ: Selenium (Chrome)
 import time
 
 from selenium import webdriver
@@ -16,12 +16,27 @@ driver.maximize_window()
 
 user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
 user_name.send_keys("standard_user")
+print("Input login")
 
 user_password = driver.find_element(By.XPATH, "//*[@id='password']")
 user_password.send_keys("secret_sauce")
+print("Input password")
 
 button_login = driver.find_element(By.ID, "login-button")
 button_login.click()
+print("Click on Login Button")
+print(driver.current_url)
+get_url = driver.current_url
+target_url = "https://www.saucedemo.com/inventory.html"
 
+assert target_url == get_url, "URL not correct"
+print("URL correct")
+
+text_products = driver.find_element(By.XPATH, "//span[@class='title']")
+print(text_products.text)
+
+value_text_products = text_products.text
+assert value_text_products == "Products", "Title not correct"
+print("Title correct")
 # time.sleep(10)
 # driver.close()
