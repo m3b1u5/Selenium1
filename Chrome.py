1213d0c405cd1c63341e6ad6847d5fd5ec9e2db4
m3b1u5 <dbatezat@gmail.com>
2024-12-28 16:19:28 +0200

    headless mode

--- Chrome.py
+++ Chrome.py
@@ -8,10 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
+options.add_argument("--headless")
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
-driver.maximize_window()
+# driver.maximize_window()
 # driver.set_window_size(1920, 1080)
 
 user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
@@ -25,7 +26,7 @@ print("Input password")
 button_login = driver.find_element(By.ID, "login-button")
 button_login.click()
 print("Click on Login Button")
-print(driver.current_url)
+print(f"Current URLS is: {driver.current_url}")
 get_url = driver.current_url
 target_url = "https://www.saucedemo.com/inventory.html"
 
@@ -33,10 +34,12 @@ assert target_url == get_url, "URL not correct"
 print("URL correct")
 
 text_products = driver.find_element(By.XPATH, "//span[@class='title']")
-print(text_products.text)
+print(f"Title is: {text_products.text}")
 
 value_text_products = text_products.text
 assert value_text_products == "Products", "Title not correct"
 print("Title correct")
-# time.sleep(10)
-# driver.close()
+
+print(f"{' All test passed! ':=^50}")
+
+driver.close()
