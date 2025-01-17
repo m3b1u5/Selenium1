4f9a14af0bc93d4c4d856e2493dd14b5bfdf7924
m3b1u5 <dbatezat@gmail.com>
2025-01-17 11:15:03 +0200

    Scrolling page

---
+++ Chrome_Scrolling.py
@@ -0,0 +1,57 @@
+# ДЗ: Selenium (Chrome)
+import time
+
+# importing requirements
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+
+# setting up driver
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url = 'https://www.saucedemo.com/'  # URL to test
+driver.get(base_url)
+# driver.maximize_window()
+driver.set_window_size(1920, 1080)
+
+# USERNAME input
+user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
+user_name.send_keys("standard_user")
+print("Input login")
+
+# PASSWORD input
+user_password = driver.find_element(By.XPATH, "//*[@id='password']")
+user_password.send_keys("secret_sauce")
+print("Input password")
+
+# LOGIN button click
+button_login = driver.find_element(By.ID, "login-button")
+button_login.click()
+print("Click on Login Button")
+print(f"Current URLS is: {driver.current_url}")
+
+# Adding items to shop cart
+button_add_backpack = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-backpack']").click()
+button_add_bike = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bike-light']").click()
+button_add_t_shirt = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-bolt-t-shirt']").click()
+button_add_jacket = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-fleece-jacket']").click()
+button_add_onesie = driver.find_element(By.XPATH, "//button[@id='add-to-cart-sauce-labs-onesie']").click()
+button_add_t_shirt_red = driver.find_element(By.XPATH, "//button[@id='add-to-cart-test.allthethings()-t-shirt-(red)']").click()
+print('All items added to shop cart')
+
+# Open Shop cart
+button_cart_link = driver.find_element(By.XPATH, "//a[@class='shopping_cart_link']").click()
+print('Shop cart opened')
+
+# Scrolling to last item in cart
+actions = ActionChains(driver)
+element = driver.find_element(By.ID, "item_3_title_link")
+actions.move_to_element(element).perform()
+print('Scrolling to the last item in cart')
+time.sleep(3)
+driver.execute_script("window/scrollTo(0,0)")
+
+driver.close()
